72f606d2dd95a25c01cb296b7640cd020b3403d3
Sabashnikov <160218448+Sabashnikov@users.noreply.github.com>
2024-05-09 15:36:51 +0300

    b169 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,36 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> Tree;
+  std::ifstream file(filename);
+  std::string strfile = "";
+  if (!file) {
+    std::cout << "Ошибка файла!" << std::endl;
+    return Tree;
+  }
+  while (!file.eof()) {
+    char ch = file.get();
+    strfile += ch;
+  }
+  for (char& c : strfile) {
+    c = std::tolower(c);
+  }
+  int length = strfile.length();
+  bool inWord = false;
+  std::string temp = "";
+  for (int i = 0; i < length; i++) {
+    if (strfile[i] >= 'a' && strfile[i] <= 'z') {
+      inWord = true; 
+    } else if (strfile[i] == ' ') {
+      inWord = false;
+    }
+    if (inWord) {
+      temp += strfile[i];
+    } else {
+      Tree.add(temp);
+      temp = "";
+    }
+  }
+  file.close();
+  return Tree;
 }
